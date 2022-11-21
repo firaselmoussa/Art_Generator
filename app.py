a3fec1e489f6345ff3c33534bd9170e7728d79f3
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-21 21:32:30 +0200

    implemented color pallette generation

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from PIL import Image
 import random
+import os
+import os.path
 
 
 def generateArt():
 
     img = Image.new("RGB", (1200, 1000), "black")
 
+    # creating collor pallette
+    colors_num = random.randint(3, 6)
+    colors = []
+    for i in range(0, colors_num):
+        colors.append((random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255), random.randint(200, 255)))
+
     for i in range(img.width):
         for j in range(img.height):
-            img.putpixel((i, j), (random.randint(0, 255), random.randint(
-                0, 255), random.randint(0, 255), random.randint(0, 255)))
+            img.putpixel((i, j), random.choice(colors))
 
     img.show()
-    img = img.save("generated_art/img1.png")
 
 
 generateArt()
